681ae164eb11169666fa1a004c2c9b6aed5fa87b
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-05-12 18:45:06 +0530

    practice done

---
+++ Fuctions.py
@@ -0,0 +1,11 @@
+str= "i am studying from apna college from"
+
+print(str.endswith("I"))
+print(str.capitalize())
+# replace strings 
+print(str.replace("o" , "a"))
+# Find function :
+print(str.find("o"))
+#  Count Occurrence :
+print(str.count("from"))
+print(str.count("o"))
