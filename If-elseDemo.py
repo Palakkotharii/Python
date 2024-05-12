adf1318ded691a06585878d04b7b309a66e57dc5
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-05-12 21:31:52 +0530

    pract

---
+++ If-elseDemo.py
@@ -0,0 +1,15 @@
+age=21
+
+if age>=18:
+    print("Can vote and apply for license")
+
+# If-elif - else 
+light= "green"
+
+if(light=="red"):
+    print("Stop")
+elif(light== "green"):
+    print("go")
+elif(light== "yellow"):     
+    print("look")
+print('End of Code ')
